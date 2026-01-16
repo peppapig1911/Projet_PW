d5835f1b32ada41f1fd2328806cf31b0e6473bcb
YE JIN Christine <christine.yjn@gmail.Com>
2026-01-16 10:41:41 +0100

    App Routes definition added

---
+++ event-frontend/src/AppRoutes.tsx
@@ -0,0 +1,47 @@
+import {useMemo} from "react";
+import {Navigate,Route,Routes} from "react-router-dom"
+import type {User} from "./utils/types"
+import LoginPage from "./Pages/LoginPage.tsx"
+import SignupPage from "./Pages/SignupPage.tsx";
+
+type AppRoutesProps={
+    user:User |null;
+};
+
+export default function AppRoutes({user,}:AppRoutesProps){
+    const token = localStorage.getItem("token");
+    const isAuthenticated = useMemo(()=>Boolean(token && user),[token,user])
+
+    return(
+        <Routes>
+            <Route path="/"
+                   element={
+                       isAuthenticated ?(
+                           <Navigate to="/events" replace/>
+                       ): (
+                           <LoginPage/>
+                       )
+                   }
+            />
+
+            <Route path="/login"
+                   element={
+                       isAuthenticated ?(
+                           <Navigate to="/events" replace/>
+                       ): (
+                            <LoginPage/>
+                       )
+                   }
+            />
+            <Route path="/signup"
+                   element={
+                       isAuthenticated ?(
+                           <Navigate to="/events" replace/>
+                       ): (
+                           <SignupPage/>
+                       )
+                   }
+            />
+        </Routes>
+    )
+}
